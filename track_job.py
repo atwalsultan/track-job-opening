db9a9be2815b55c93b8530ce8bcb389a833299aa
Sultan <sultan.singh.atwal@gmail.com>
2021-04-17 10:12:50 -0700

    feat: check relevant roles

--- track_job.py
+++ track_job.py
@@ -6,6 +6,14 @@ import socket
 
 import datetime
 
+from selenium import webdriver
+
+from selenium.webdriver.support.ui import WebDriverWait
+
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver.common.by import By
+
 # Log outcome to text file every time the program is run
 
 
@@ -39,8 +47,61 @@ def is_connected():
     return False
 
 
+def check_relevant_roles():
+    site_url = 'https://langara.wd10.myworkdayjobs.com/External_Employment_Opportunities'
+
+    # Start WebDriver and load page
+    log('Loading page...')
+    wd = webdriver.Chrome()
+    wd.get(site_url)
+
+    # Wait for dynamically loaded elements
+    WebDriverWait(wd, 10).until(EC.visibility_of_element_located(
+        (By.ID, "wd-FacetedSearchResultList-facetSearchResultList.newFacetSearch.Report_Entry")))
+
+    # Fetch and parse HTML content
+    log('Fetching and parsing HTML...')
+    html_page = wd.page_source
+    wd.quit()
+    soup = BeautifulSoup(html_page, 'lxml')
+
+    # Get relevant jobs
+    log('Looking for relevant jobs...')
+    job_soup = soup.find('div', {
+                         'id': 'wd-FacetedSearchResultList-facetSearchResultList.newFacetSearch.Report_Entry'})
+
+    roles_soup = job_soup.find_all('div', class_='gwt-Label')
+
+    roles = []
+    for role in roles_soup:
+        # if(('WOC' in role.get_text() and '2021' in role.get_text()) or ('SWAP' in role.get_text() and '2021' in role.get_text())):
+        if('Instructor' in role.get_text()):
+            roles.append(role.get_text())
+
+    return(roles)
+
+
 if(is_connected()):
-    log('Internet connection available.')
+    try:
+        log('Internet connection available.')
+        relevant_roles = check_relevant_roles()
+
+        if(relevant_roles):
+            log(f'{len(relevant_roles)} relevant roles(s) found.')
+
+            # Send mail
+
+        else:
+            log('No relevant roles found')
+
+    except Exception as e:
+        log(f'Error: {str(e)}.')
+
+        # try:
+        # Send error mail
+
+        # If error mail cant be sent
+        # except Exception as e:
 
 else:
     log('No internet connection.')
